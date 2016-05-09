a384a310f50aa8536c197c8634323f73c72bed5c
timwalsh300 <timwalsh300@gmail.com>
2016-05-09 07:56:12 -0500

    Lots of new images for the GUI. Need to build the custom ComboBox next.

--- src/robots/RobotsDocumentController.java
+++ src/robots/RobotsDocumentController.java
@@ -7,6 +7,8 @@ package robots;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -32,33 +34,49 @@ public class RobotsDocumentController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
 
+        ObservableList<String> spaceOptions
+                = FXCollections.observableArrayList(
+                        " ",
+                        "Red Star",
+                        "Red Moon",
+                        "Red Gear",
+                        "Red Planet",
+                        "Blue Star",
+                        "Blue Moon",
+                        "Blue Gear",
+                        "Blue Planet",
+                        "Yellow Star",
+                        "Yellow Moon",
+                        "Yellow Gear",
+                        "Yellow Planet",
+                        "Green Star",
+                        "Green Moon",
+                        "Green Gear",
+                        "Green Planet"
+                );
+
         for (int row = 0; row < 31; row++) {
             for (int column = 0; column < 31; column++) {
-                if (row >= 13 && row <= 17 && column >=13 && column <= 17) {
-                    Label bigcenterlabel = new Label();
-                    boardGrid.add(bigcenterlabel, column, row);
-                }
-                else if (row % 2 == 0) {
+                if (row >= 13 && row <= 17 && column >= 13 && column <= 17) {
+                    boardGrid.add(new Label(), column, row);
+                } else if (row % 2 == 0) {
                     if (column % 2 == 0) {
-                        boardGrid.add(new ComboBox(), column, row);
-                    }
-                    else {
+                        ComboBox spaceComboBox = new ComboBox(spaceOptions);
+                        
+                        boardGrid.add(spaceComboBox, column, row);
+                    } else {
                         ToggleButton vbarrier = new ToggleButton();
                         vbarrier.getStyleClass().add("vbarrier");
                         boardGrid.add(vbarrier, column, row);
                     }
-                }
-                else {
-                    if (column % 2 == 0) {
-                        ToggleButton hbarrier = new ToggleButton();
-                        hbarrier.getStyleClass().add("hbarrier");
-                        boardGrid.add(hbarrier, column, row);
-                    }
-                    else {
-                        Label cornerlabel = new Label();
-                        cornerlabel.getStyleClass().add("cornerlabel");
-                        boardGrid.add(cornerlabel, column, row);
-                    }
+                } else if (column % 2 == 0) {
+                    ToggleButton hbarrier = new ToggleButton();
+                    hbarrier.getStyleClass().add("hbarrier");
+                    boardGrid.add(hbarrier, column, row);
+                } else {
+                    Label cornerlabel = new Label();
+                    cornerlabel.getStyleClass().add("cornerlabel");
+                    boardGrid.add(cornerlabel, column, row);
                 }
             }
         }
